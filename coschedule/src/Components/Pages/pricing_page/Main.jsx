e3342a10ff06a6554f89b957fa5beab60d3cd1c0
pri65 <priyankasoniii65@gmail.com>
2022-08-26 21:52:14 +0530

    new

--- coschedule/src/Components/Pages/pricing_page/Main.jsx
+++ coschedule/src/Components/Pages/pricing_page/Main.jsx
@@ -4,37 +4,145 @@ import  "./main.css";
 const Main = () => {
   return (
     <div>
-        {/* <div style={{backgroundColor:"#f8f8f8;", height:"700px"}}> */}
         <div className='heading1'>
-            
             <img src='https://coschedule.com/img/cos-logo-full-color.svg' alt=''/>
-            
         </div>
         <div className='heading2'>
             <h1>Choose Your All-In-One Marketing Calendar</h1>
-            <p>Create your calendar free today. No credit card&nbsp; required.</p>
+            <p >Create your calendar free today. No credit card&nbsp; required.</p>
         </div>
         <div className='flex'>
             <div className='box'>
+                <img className='img1' src='https://coschedule.com/img/pricing/mc-free-badge-01.png' alt='free' />
                 <div className='plan-title'>Free</div>
                 <h2 className='plan-name'>Marketing calender</h2>
-                <p>The forever-free marketing calendar to see everything in one place.</p>
+                <p style={{padding:"40px", marginLeft:"10px",marginTop:"-40px"}}>The forever-free marketing calendar to see everything in one place.</p>
                 <br />
                 <br />
                 <br />
                 <h1>$0</h1>
                 <h3>Free Forever</h3>
-                <button>create My Free Calender</button>
-                <p>Marketing Calender Free includes:</p>
+                <br />
+                <br />
+                <br />
+                <button>create My Free Calender</button> 
+                <p className='ulTitle'>Marketing Calender Free includes:</p>
                 <ul>
                     <li>
-                        Personal project & Task Management
+                    Personal project & Task Management
+                    </li>
+                    <li>
+                    Unlimited Marketing Projects and Content
+                    </li>
+                    <li>
+                    Marketing & Project Management Integrations
+                    </li>
+                    <li>
+                    Social Media Publishing Up to 2 Profiles
+                    </li>
+                    <li>
+                    Self-Serve Knowledge Base
+                    </li>
+                    <li>
+                    Basic Support
                     </li>
                 </ul>
+                <button className='btn2'>Create My Free Calender</button>
             </div>
-            <div className='box'></div>
-            <div className='box'></div>
+            <div className='box'>
+                
+                <div className='plan-title'>Pro</div>
+                <h2 className='plan-name'>Marketing calender</h2>
+                <p style={{padding:"40px", marginLeft:"10px",marginTop:"-40px"}}>An advanced marketing calendar with limitless publishing.</p>
+                <br />
+                <br />
+                <br />
+                <h1>$29</h1>
+                <h3>per user / month</h3>
+                <br />
+                <br />
+                <br />
+                <button>Get Started</button>
+                
+                <p className='ulTitle'>Includes all Marketing Calendar Free capabilities:</p>
+                <ul>
+                    <li>
+                    Unlimited, Shareable Read-Only Calendar Views
+                    </li>
+                    <li>
+                    Team Task Management & Collaboration
+                    </li>
+                    <li>
+                    Recurring Tasks & Events
+                    </li>
+                    <li>
+                    Customizable Task Workflow Templates
+                    </li>
+                    <li>
+                    Bulk Social Media Scheduling
+                    </li>
+                    <li>
+                    Social Media Automation
+                    </li>
+                    <li>
+                    Social Media Campaign Templates
+                    </li>
+                </ul>
+                <button className='btn2'>Get Started</button>
+            </div>
+            <div className='box'>
+               
+                <div className='plan-title'>Free</div>
+                <h2 className='plan-name'>Marketing Suite</h2>
+                <p style={{padding:"40px", marginLeft:"10px",marginTop:"-40px"}}>A family of agile marketing products that helps you coordinate your process, projects, and teams.</p>
+                <br />
+                <br />
+                <br />
+                <h1>Let's Talk</h1>
+                <br />
+                <br />
+                <br />
+                <button>Contact Sales</button>
+                <p className='ulTitle'>Includes all Marketing Calendar Pro capabilities, plus:</p>
+                <ul>
+                    <li>
+                    Marketing Campaigns
+                    </li>
+                    <li>
+                    Sub-Calendars
+                    </li>
+                    <li>
+                    Request Forms
+                    </li>
+                    <li>
+                    Custom Permissions & User Access
+                    </li>
+                    <li>
+                    Approval Workflows
+                    </li>
+                    <li>
+                    Project & Campaign Templates
+                    </li>
+                    <li>
+                    And more!
+                    </li>
+                </ul>
+                <button className='btn2'>Contact Sales</button>
+            </div>
         </div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
         
         <hr /> 
         <div className='endLine'>
@@ -54,7 +162,36 @@ const Main = () => {
                 </p>
             </div>
         </div>
-        <div className='faq'></div>
+        <div className='faq'>
+            <h3>FAQ's</h3>
+            <hr />
+            <div className='pricing-faq-wrapper'>
+                <div className='pricing-faq'>
+                    <h3>How do I upgrade from a Free to a Pro plan?</h3>
+                    <p>It’s easy! Just visit the billing page in your account. There you can select a new plan. Once you complete the billing prompts, your account will automatically upgrade to Pro & unlock all your new features.</p>
+                </div>
+                <div className='pricing-faq'>
+                    <h3>How do I get started with Marketing Suite?</h3>
+                    <p>To get started, schedule your demo of the CoSchedule Marketing Suite. You’ll get 1:1 time with a CoSchedule expert to see how it can help you coordinate your team, projects, & process.</p>
+                </div>
+                <div className='pricing-faq'>
+                    <h3>Do you offer discounts for nonprofits?</h3>
+                    <p>To make it easier for your nonprofit to organize all of your marketing with CoSchedule, we offer a 30% discount on all of our paid plans. Learn more.</p>
+                </div>
+                <div className='pricing-faq'>
+                    <h3>Which payment types do you accept?</h3>
+                    <p>Plastic is fantastic. We accept credit card payments for all of our paid plans. Credit cards are not required for Marketing Calendar Free. We may also accept other payment types for Marketing Suite. Check with your account executive for details. For more information on billing, visit our Billing FAQs support doc.</p>
+                </div>
+                <div className='pricing-faq'>
+                    <h3>I’m just getting started. How can I learn more about CoSchedule?</h3>
+                    <p>If you are using Marketing Calendar Free or Marketing Calendar Pro, check out our Getting Started Guides to learn the ins and outs of using your new calendar. If you’re in a Marketing Suite plan, you’ll get personalized onboarding & support from your dedicated customer success manager.They’ll be in touch, and they’re excited to help you & your team reach your goals.</p>
+                </div>
+                <div className='pricing-faq'>
+                    <h3>Where can I find other helpful resources?</h3>
+                    <p>Our guides will show you how to get organized with your new CoSchedule calendar. Review our Power Tips for inspiration. Or visit the Actionable Marketing Institute to learn how to improve your marketing.</p>
+                </div>
+            </div>
+        </div>
         <div className='footer'></div>
     </div>
   )

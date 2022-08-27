c803b3114dc5a229b2dd4fa7aab29ed4f8848ad0
suraj sangle <surajsangale41198@gmail.com>
2022-08-27 20:38:28 +0530

    day5

---
+++ coschedule/src/Components/Routes/Home.jsx
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+ import Navbar from "../Pages/Navbar"
+ import Footer from "../Pages/Footer"
+import Homepage from "../Pages/HomePage"
+
+function Home() {
+    
+
+  return (
+    <div>
+    
+      <Navbar/>
+      <Homepage/> 
+      <Footer/>  
+      
+    
+    </div>
+  );
+}
+export default Home;

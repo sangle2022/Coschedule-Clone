5e160580d7886546f30d5993a31fd664768bcb0a
Vaibhav Sultane <vaibhavsultane1997@gmail.com>
2022-10-10 13:21:43 +0530

    Update db.js

--- coschedule/backend/Config/db.js
+++ coschedule/backend/Config/db.js
@@ -3,7 +3,7 @@ const mongoose=require("mongoose")
 require('dotenv').config()
 
 const connection=mongoose.connect(process.env.MONGO_URL)
-// mongodb+srv://vaibhav2416:vaibhav123@cluster0.kki49hi.mongodb.net/?retryWrites=true&w=majority
+
 
 module.exports={
     connection
